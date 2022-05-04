4256871b3a75b0645d5860712947012693c83696
Cesar Martinez <cmartinezaraujo@ewu.edu>
2022-05-03 17:48:58 -0700

    Analog clock
    *Added asset for clock
    *Added markup for clock 
    *Adjusted JS for clock movement
    *Adjusted style for clock

--- js/time.js
+++ js/time.js
@@ -1,62 +1,20 @@
-/************
-Dynamic Data
-************/
-// define `theClockApp` stored in function to be ran
+const deg = 6;
+const hr = document.querySelector('#hr');
+
+const mn = document.querySelector('#mn');
+
+const sc = document.querySelector('#sc');
+
+
 const theClockApp = () => {
-  
-// Get and store current date and time with `new Date()` object
-const timeNow = new Date();
-// Check-Check: See all `Date()` methods and properties
-// console.log( timeNow );
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
 
-// Get current hours
-let gotHours = timeNow.getHours();
-// Get current minutes
-let gotMinutes = timeNow.getMinutes();
-// Get current seconds
-let gotSeconds = timeNow.getSeconds();
-
-// Check-Check: Is the data correct? 
-// console.log(gotHours, gotMinutes, gotSeconds);
-
-/************
-Format Data
-************/
-// Get AM or PM
-const gotAmOrPM = gotHours >= 12 ? 'PM' : 'AM';
-// Get 12 hour format
-gotHours = gotHours % 12 || 12;
-// Optionally, append zero to single digit hours
-gotHours = gotHours < 10 ? `0${gotHours}` : gotHours;   
-// Optionally, append zero to single digit minutes
-gotMinutes = gotMinutes < 10 ? `0${gotMinutes}` : gotMinutes;
-// Optionally, append zero to single digit seconds
-gotSeconds = gotSeconds < 10 ? `0${gotSeconds}` : gotSeconds;
-
-/************
-Get DOM Elements
-************/
-// Get Hours
-const hours = document.querySelector(".hours");
-// Get minutes
-const minutes = document.querySelector(".minutes");
-// Get seconds
-// const seconds = document.querySelector(".seconds");
-// Get AM or PM
-const amOrPM = document.querySelector('.amOrPM');
-
-/************
-Set DOM Elements
-************/
-// Set the hours
-hours.innerText = gotHours;
-// Set the minutes
-minutes.innerText = gotMinutes;
-// Set the seconds
-// seconds.innerText = gotSeconds;
-// Set AM or PM
-amOrPM.innerText = gotAmOrPM;
-  
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
 }
 
 /************
